1ad35fc605ed1baecf9b8737e3b2bbcc00baf6bb
Ashley <ashley@thepaynes.uk>
2023-03-16 13:41:36 +0000

    Get Adjacent Tiles

--- main.ts
+++ main.ts
@@ -1,1 +1,31 @@
+//% weight=0 color=#13a89e icon="\uf041" block="Tiles Advanced"
+//% advanced=false
+//% groups="['Local Tiles', 'Game Population']"
 
+namespace tilesAdvanced {
+
+    //% blockId=getAdjacentTiles
+    //% block="get tiles near to $tile within $distance"
+    //% group="Local Tiles"
+    //% weight=20
+    export function getAdjacentTiles(tile: tiles.Location, distance: number): tiles.Location[] {
+        let i: number;
+        let col = tile.col;
+        let row = tile.row;
+        let adjacent_tiles = [tile];
+
+        for (i = 0; i < distance; i++) {
+            adjacent_tiles.push(tiles.getTileLocation(col - i, row))
+        }
+        for (i = 0; i < distance; i++) {
+            adjacent_tiles.push(tiles.getTileLocation(col + i, row))
+        }
+        for (i = 0; i < distance; i++) {
+            adjacent_tiles.push(tiles.getTileLocation(col, row - i))
+        }
+        for (i = 0; i < distance; i++) {
+            adjacent_tiles.push(tiles.getTileLocation(col, row + i))
+        }
+        return adjacent_tiles
+    }
+}
