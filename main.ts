b09686f712be8cf31bddc65d28cb664b4d5b0aa1
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-03-16 21:17:27 +0000

    Added the tileIsInList function

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //% weight=0 color=#13a89e icon="\uf041" block="Tiles Advanced"
 //% advanced=false
-//% groups="['Local Tiles', 'Game Population']"
+//% groups="['Local Tiles', 'Game Population', 'Tile Comparisons]"
 
 namespace tilesAdvanced {
     /**
@@ -29,5 +29,21 @@ namespace tilesAdvanced {
             adjacent_tiles.push(tiles.getTileLocation(col, row + i))
         }
         return adjacent_tiles
+    }
+
+    /**
+     * Returns true if the given tile is in the list of tiles provided
+     */
+    //% blockId=tileIsInList
+    //% block="$tile is in $tileList"
+    //% group="Tile Comparisons"
+    //% weight=20
+    export function tileIsInList(tile: tiles.Location, tileList: tiles.Location[]): boolean{
+        for (let tileInList of tileList){
+            if (tile.col == tileInList.col && tile.row == tileInList.row){
+                return true
+            }
+        }
+        return false
     }
 }
