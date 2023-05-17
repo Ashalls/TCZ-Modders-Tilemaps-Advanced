1a670de0d49bc759c8c19eab4b8ae67e51eb6cda
Tom Moran <62551154+Tomhausen@users.noreply.github.com>
2023-05-17 11:00:25 +0100

    cleared main

--- main.ts
+++
@@ -1,9 +0,0 @@
-tiles.setCurrentTilemap(tilemap`map`);
-
-let mySprite = sprites.create(assets.image`player`, SpriteKind.Player);
-controller.moveSprite(mySprite);
-scene.cameraFollowSprite(mySprite)
-
-let myEnemy = sprites.create(assets.image`enemy`, SpriteKind.Enemy);
-tiles.placeOnRandomTile(myEnemy, assets.tile`ground`)
-tilesAdvanced.followUsingPathfinding(myEnemy, mySprite, 50)
